4d8fe6ea0f41d4127bedcef2a80f064062eff842
Raviv Herrera <ravivhe94@gmail.com>
2023-06-13 13:54:35 +0300

    add files

---
+++ main.py
@@ -0,0 +1,13 @@
+from rigid_body_trans_class import RigidBodyTrans
+from dataset_examples import Torus
+from viewer import Viewer3D
+
+if __name__ == "__main__":
+
+    t1 = Torus(num_of_points=100)
+    # t2 = Torus(num_of_points=100)
+    t2 = t1.create_shifted_torus()
+    rbt_instance = RigidBodyTrans(p=t1.torus, q=t2.torus)
+    rbt_instance.calc_transformation()
+    Viewer3D.plot(t1, t2, rbt=rbt_instance)
+    print("")
